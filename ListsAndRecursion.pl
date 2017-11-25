670b7e08edc3b17722e557dfad7242e80b0e6d2d
Maximilian Wahl <mexx.wale@gmail.com>
2017-11-25 13:47:33 +0100

    file added

---
+++ ListsAndRecursion.pl
@@ -0,0 +1,8 @@
+prepend(E,[],[E]).
+prepend(E,[H|T],[E|[H |T]]).
+addElement(E,[],[E]).
+addElement(E,[H|T],[H|ExtendedTail]):-addElement(E,T,ExtendedTail).
+haslength([],0).
+haslength([_|T],X):- haslength(T,Y),X is 1+ Y.
+remove(E,[E|Tail],Tail).
+remove(E,[_|Tail],ReducedTail):-remove(E,Tail,ReducedTail).
